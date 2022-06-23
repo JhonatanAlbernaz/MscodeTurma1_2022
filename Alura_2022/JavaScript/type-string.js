c11bdee419774fa622bc5f9982dd816ef5661a45
JhonatanAlbernaz <jhonatanalbernaz124@gmail.com>
2022-06-23 14:35:16 -0300

    MsCode

---
+++ Alura_2022/JavaScript/type-string.js
@@ -0,0 +1,19 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumero = "34567";
+
+const citacao = 'O Jhonatan disse "oi!"';
+console.log(citacao);
+
+//===============================================//
+
+//Concatenação (+)
+
+const citacaoNaConcatenacao = "Meu nome é ";
+const meuNome = "Jhonatan";
+console.log(citacaoNaConcatenacao + meuNome);
+
+//===============================================//
+
+//Template String Ou Template Literal
